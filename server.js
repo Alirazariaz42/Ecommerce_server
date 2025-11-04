ec966b95b1e83b84a755b87209a2cd2edf88a2d0
demogitHub <alirazabt15342@gmail.com>
2025-11-04 13:20:57 +0500

    remove the db connection function from server file

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from "express";
-import connectDB from "./config/db_config.js";
 const app = express();
 
-connectDB();
 app.use("/", (req, res) => {
   res.send("server is running  on 3000 port");
 });
